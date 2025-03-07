6c3a3effc149e66f8bb9f671c635df0902b0d055
Alondra <'medellinalondra70@gmail.com'>
2025-03-06 20:19:40 -0600

    06/Marzo/2025 Alondra Creacion del Repositorio Subir todos los archivos y carpetas iniciales

---
+++ src/logic/Compiler.java
@@ -0,0 +1,17 @@
+package logic;
+
+import gui.Screen;
+
+/*
+ * @author Alondra
+ * @author Daniel
+ * @author Many
+ */
+
+public class Compiler {
+    public static void main(String[] args) {
+        Screen obs = new Screen();
+        obs.setVisible(true);
+        obs.setLocationRelativeTo(null);
+    }
+}
